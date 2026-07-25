eddb4d6535061fdc9d6611547b126b7f5a48043c
LucasWills <lucasbwills@gmail.com>
2026-07-25 01:27:12 -0700

    working on layout..,,

--- src/main.js
+++ src/main.js
@@ -1,60 +1,55 @@
-import './style.css'
+import '/style.css'
 import javascriptLogo from './assets/javascript.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
+document.querySelector('#app').innerHTML = /*html*/`
+<h style="text-align: left">Lucas Wills</h>
 <section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${javascriptLogo}" class="framework" alt="JavaScript logo"/>
-    <img src="${viteLogo}" class="vite" alt="Vite logo" />
-  </div>
+  <h1>Welcome to my Hobby Site!</h1>
+  <!-- <a href="/login/">login</a> -->
+</section>
+
+<div class="ticks"></div>
+
+<!-- categories -->
+<section style="text-align: left">
+  <h1>Categories</h1>
+
+  <div class="div-line"></div>
+
   <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.js</code> and save to test <code>HMR</code></p>
+    <h2>High Voltage Electronics</h2>
   </div>
-  <button id="counter" type="button" class="counter"></button>
+
+  <div class="div-line"></div>
+
+  <div>
+    <h2>Misc. Electronics</h2>
+  </div>
+  
+  <div class="div-line"></div>
+
+  <div>
+    <h2>Robotics</h2>
+  </div>
+
+  <div class="div-line"></div>
+
+  <div>
+    <h2>Chemsitry</h2>
+  </div>
+
+  <div class="div-line"></div>
+
+  <div>
+    <h2>Programming</h2>
+  </div>
+
+  <div class="div-line"></div>
+
 </section>
 
-<div class="ticks"></div>
-
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank">
-          <img class="logo" src="${viteLogo}" alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-          <img class="button-icon" src="${javascriptLogo}" alt="">
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-      <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-      <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-      <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-    </ul>
-  </div>
-</section>
-
-<div class="ticks"></div>
-<section id="spacer"></section>
+<div class="div-line"></div>
 `
 
-setupCounter(document.querySelector('#counter'))
