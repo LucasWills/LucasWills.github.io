081fc550e192285f81448b0fdcf080f304842a1f
LucasWills <lucasbwills@gmail.com>
2026-07-25 13:55:52 -0700

    working link to qcw page

--- src/main.js
+++ src/main.js
@@ -4,45 +4,82 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 
 document.querySelector('#app').innerHTML = /*html*/`
+
+
+
+<!-- top header stuff -->
+
 <h style="text-align: left">Lucas Wills</h>
+
 <section id="center">
-  <h1>Welcome to my Hobby Site!</h1>
+  <h1>Welcome to my WORK IN PROGRESS Hobby Site!</h1>
   <!-- <a href="/login/">login</a> -->
 </section>
 
-<div class="ticks"></div>
+
+
 
 <!-- categories -->
-<section style="text-align: left">
+
+
+<section id="categories-list">
   <h1>Categories</h1>
 
   <div class="div-line"></div>
 
-  <div>
-    <h2>High Voltage Electronics</h2>
+  <div class="category">
+    <h2><b>High Voltage Electronics</b></h2>
+
+    <ul class="page-list">
+      <b class="page-list-category">Tesla Coils (Kilohertz range)</b>
+      <li><a href="/HV/QCW1/">QCWDRSSTC</a> - Quasi-Continuous Wave solid state tesla coil, achieved 6 foot arcs!</li>
+      <li><a href="/">DRSSTC 2</a> - 2.5" diameter dual-resonant solid state tesla coil, acieved 2 foot arcs.</li>
+      <li><a href="/">Misc. SSTCs</a> - Other solid state tesla coil projects!</li>
+
+      <b class="page-list-category">High-Frequency Solid State Tesla Coils (Megahertz range)</b>
+      <li><a href src="/">Self-Oscillating HFSSTCs</a> - Other tesla coil projects!</li>
+      <li><a href src="/">Fixed Frequency HFSSTCs</a> - Other tesla coil projects!</li>
+      <li><a href src="/">Interrupted PLL Driven HFSSTC</a> - Other tesla coil projects!</li>
+
+      <b class="page-list-category">Inductively-Coupled Plasma (ICP)</b>
+      <li><a href src="/">Plasma Toroid</a> - Other tesla coil projects!</li>
+      <li><a href src="/">Radio Frequency HV Oscillator for ICP</a> - Other tesla coil projects!</li>
+      
+      <b class="page-list-category">High Voltage Power Supplies</b>
+      <li><a href src="/">Small Ferrite HV Transformers</a> - Other tesla coil projects!</li>
+      <li><a href src="/">HUGE Ferrite HV Transformer</a> - Other tesla coil projects!</li>
+      <li><a href src="/">HV Capacitor-Charging-Power-Supply</a> - Other tesla coil projects!</li>
+
+
+
+
+
+
+
+    </ul>
   </div>
 
   <div class="div-line"></div>
 
-  <div>
+  <div class="category">
     <h2>Misc. Electronics</h2>
   </div>
   
   <div class="div-line"></div>
 
-  <div>
+  <div class="category">
     <h2>Robotics</h2>
   </div>
 
   <div class="div-line"></div>
 
-  <div>
+  <div class="category">
     <h2>Chemsitry</h2>
   </div>
 
   <div class="div-line"></div>
 
-  <div>
+  <div class="category">
     <h2>Programming</h2>
   </div>
 
