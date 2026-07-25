6d30b41caf7924a7588e730bb6dd6653fd9cec24
LucasWills <lucasbwills@gmail.com>
2026-07-25 14:02:24 -0700

    actually working links this time

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+
 
 export default defineConfig({
-  base: '/'
+  base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        QCW1: resolve(__dirname, 'HV/QCW1/index.html')
+      }
+    }
+
+  }
 })
